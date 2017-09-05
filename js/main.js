3dec5763dd030d7bf81d58445b0388454ac689b1
Jared Pleva <jpleva91@gmail.com>
2017-09-04 21:03:55 -0600

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
-var card0ne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-console.log("User flipped " + card0ne);
-console.log("User flipped " + cardFour);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+var cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} 
+	else {
+		alert("Sorry, try again.");
+	}
+}
